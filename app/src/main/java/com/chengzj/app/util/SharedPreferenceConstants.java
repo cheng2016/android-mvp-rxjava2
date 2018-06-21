d0f98089163840acab068ab2e339d3603cd68c96
chengzj <1102743539@qq.com>
2018-06-21 14:48:47 +0800

    添加SharedPreference 和 Toast 工具类

---
+++ app/src/main/java/com/chengzj/app/util/SharedPreferenceConstants.java
@@ -0,0 +1,14 @@
+package com.chengzj.app.util;
+
+/**
+ * Created by chengzj on 2017/6/21.
+ */
+public class SharedPreferenceConstants {
+    public final static String USER_NAME = "user_name";
+    public final static String USER_PASSWORD = "user_password";
+
+    public final static String ACCESS_TOKEN = "refreshToken";
+    public final static String REFRESH_TOKEN = "refreshToken";
+    public final static String OPENID = "openid";
+    public final static String UNIONID = "unionid";
+}
